64237fa0a2579a913b2b835f19c9daa560278934
Róbert Vári <mail.robertvari@gmail.com>
2021-01-09 17:05:44 +0100

    update

--- 06_conditions.py
+++ 06_conditions.py
@@ -1,8 +1,26 @@
 age = 10
 
-if age > 10:
+if age != 10:
     print("You are older than me!")
-elif age == 10:
-    print("We have the same age.")
+
+# if age >= 10:
+
+# if age <= 10:
+
+username = "robert"
+password = "testpas123"
+
+your_user = input("username:")
+your_password = input("password:")
+
+#           True                      True
+if username == your_user and password == your_password:
+    print("You are logged in!")
 else:
-    print("You are younger than me!")
+    print("Invalid credentials provided. Try again")
+
+#           False                      True
+if username == your_user or password == your_password:
+    print("Username or password was invalid but you are logged in anyway...")
+else:
+    print("Invalid credentials provided. Try again")
