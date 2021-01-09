0948b87ae84a2da90ad2166b17a12801ff987fff
Róbert Vári <mail.robertvari@gmail.com>
2021-01-09 14:38:12 +0100

    update

---
+++ 02_string_methods.py
@@ -0,0 +1,11 @@
+# upper()
+print("Hello World".upper())
+
+# lower()
+print("Hello World".lower())
+
+# title()
+print("vári róbert".title())
+
+# replace()
+print("Hello World".replace("World", "Robert"))
