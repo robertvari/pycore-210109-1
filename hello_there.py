4d64ff7b45e766f87f18c273605962b9e3467b80
Róbert Vári <mail.robertvari@gmail.com>
2021-01-09 16:21:57 +0100

    update

---
+++ hello_there.py
@@ -0,0 +1,16 @@
+import time
+
+print("Hello there!")
+time.sleep(2)
+
+your_name = input("What is your name?")
+
+time.sleep(2)
+
+your_age = input("How old are you?")
+
+time.sleep(1)
+
+print(f"Your name is: {your_name}. You are {your_age} old.")
+
+input()
