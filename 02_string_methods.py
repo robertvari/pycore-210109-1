619cac3d9cdffd95a0a149c885b867928522b38f
Róbert Vári <mail.robertvari@gmail.com>
2021-01-09 14:49:13 +0100

    update

--- 02_string_methods.py
+++ 02_string_methods.py
@@ -9,3 +9,9 @@ print("vári róbert".title())
 
 # replace()
 print("Hello World".replace("World", "Robert"))
+
+# format()
+print("My name is {}. I'm {} years old".format("Vári Róbert", 42))
+
+# formated string
+print(f"My name is {'Vári Róbert'}. I'm {42} years old.")
