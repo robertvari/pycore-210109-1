4a42526bd86dc5f9dcf6acfc4344605d098c72d8
Róbert Vári <mail.robertvari@gmail.com>
2021-01-09 15:56:55 +0100

    update

--- 05_dictionary.py
+++ 05_dictionary.py
@@ -2,7 +2,6 @@ my_list = ['Robert', 42, 3.14, 'Robert']
 
 
 phonebook = {
-
     "06 20 555 6677": {
         "name": "Robert Vari",
         "address": "Debrecen Fő utca 1."
@@ -14,7 +13,16 @@ phonebook = {
     }
 }
 
-print(
-    phonebook["06 30 666 7788"]["name"],
-    phonebook["06 30 666 7788"]["address"]
-)
+
+# add item to dictionary
+phonebook["06 20 999 8877"] = {
+    "name": "Kiss Krisztina",
+    "address": "Eger Fő utca 12."
+}
+
+print(phonebook)
+
+# delete item
+del phonebook["06 20 999 8877"]
+
+print(phonebook)
