3f60bb46b51ea4964588a7254acc1725d35ca411
Róbert Vári <mail.robertvari@gmail.com>
2021-01-09 15:11:59 +0100

    update

--- 03_variables.py
+++ 03_variables.py
@@ -1,7 +1,11 @@
-
 # name       value
 my_name = "Kiss Csaba"
 age = 42
 
 print(f"My name is {my_name}. I'am {age} years old.")
 
+
+# override variable
+my_name = "Robert Vari"
+age = 60
+print(f"My name is {my_name}. I'am {age} years old.")
