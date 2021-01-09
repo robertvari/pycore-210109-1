fbaa09a6774d1870ded164b3ff47ad93b32a9522
Róbert Vári <mail.robertvari@gmail.com>
2021-01-09 15:32:16 +0100

    update

--- 04_lists.py
+++ 04_lists.py
@@ -1,17 +1,23 @@
-# list
-age = 42
-fruits = ["banana", "apple", "lemon"]
-names = [
-    "Robert",
-    "Csaba",
-    "Kriszta",
-    "Csilla",
-    42,
-    3.14,
-    True,
-    False,
-    age,
-    fruits
-]
+# lists
+#           0         1
+names = ["Csaba", "Robert", "Csaba"]
+
+
+# add item
+names.append("Csilla")
+names.append("Tamás")
+names.append("István")
+names.append("Kriszta")
 
 print(names)
+print(names[-2])
+
+# remove()
+names.remove("Csaba")
+print(names)
+
+del names[-1]
+print(names)
+
+my_name = "Robert Vari"
+print(my_name[1])
