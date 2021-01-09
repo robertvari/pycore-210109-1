d8103434f69e46b4fe06c40e4e93e85260104af3
Róbert Vári <mail.robertvari@gmail.com>
2021-01-09 15:18:57 +0100

    update

---
+++ 04_lists.py
@@ -0,0 +1,17 @@
+# list
+age = 42
+fruits = ["banana", "apple", "lemon"]
+names = [
+    "Robert",
+    "Csaba",
+    "Kriszta",
+    "Csilla",
+    42,
+    3.14,
+    True,
+    False,
+    age,
+    fruits
+]
+
+print(names)
