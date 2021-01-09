de0240809bef82ae09711f6549c188486bac098b
Róbert Vári <mail.robertvari@gmail.com>
2021-01-09 16:31:05 +0100

    update

---
+++ my_phonebook.py
@@ -0,0 +1,14 @@
+print("=" * 50, "My Phonebook", "=" * 50,)
+
+phonebook = {}
+
+name = input("Name:")
+phone = input("Phone:")
+address = input("Addres:")
+
+phonebook[phone] = {
+    "name": name,
+    "address": address
+}
+
+print(phonebook)
