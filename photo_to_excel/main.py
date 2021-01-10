8e9801ad8e5730f4d306b5518e7650ca492fad48
Róbert Vári <mail.robertvari@gmail.com>
2021-01-10 16:17:29 +0100

    update

--- photo_to_excel/main.py
+++ photo_to_excel/main.py
@@ -3,7 +3,10 @@ import os
 folder_path = r"D:\Photos\CIW"  # raw string
 
 # list comprehension
-valid_photos = [photo for photo in os.listdir(folder_path) if photo.lower().endswith(".jpg")]
+photo_list = [
+    photo for photo in os.listdir(folder_path)
+    if photo.lower().endswith(".jpg")
+]
 
-print(valid_photos)
-print( len(valid_photos) )
+print(photo_list)
+print( len(photo_list) )
