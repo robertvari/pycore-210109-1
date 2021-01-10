22ea9a457a521b39399cb453a4f91714c9980cd6
Róbert Vári <mail.robertvari@gmail.com>
2021-01-10 15:46:48 +0100

    update

--- photo_to_excel/main.py
+++ photo_to_excel/main.py
@@ -1,1 +1,14 @@
-print("Hello")
+import os
+
+folder_path = r"D:\Photos\CIW"  # raw string
+photos = os.listdir(folder_path)
+
+valid_photos = []
+for item in photos:
+    if not item.lower().endswith(".jpg"):
+        continue
+
+    valid_photos.append(item)
+
+print(valid_photos)
+print( len(valid_photos) )
