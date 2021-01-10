c87f04bfc385accc6fa7e492d10f91cfa332dd27
Róbert Vári <mail.robertvari@gmail.com>
2021-01-10 17:20:41 +0100

    update

--- photo_to_excel/main.py
+++ photo_to_excel/main.py
@@ -25,7 +25,20 @@ for index, file_name in enumerate(photo_list):
 
     img = Image.open(file_path)
 
+    # add data to dimension column
     sheet[f"F{index + 2}"] = f"{img.size[0]}x{img.size[1]}"
 
+    exif_data = img._getexif()  # Sometimes None
+    if not exif_data:
+        continue
+
+    print(file_name)
+    for key, value in exif_data.items():
+        tag_name = ExifTags.TAGS.get(key)
+
+        print(f"\t{tag_name}: {value}")
+
+    break
+
 excel_file = "photo_data.xlsx"
-workbook.save(excel_file)
+# workbook.save(excel_file)
