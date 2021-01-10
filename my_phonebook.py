81e7654506f270a179efc68c4513dc6d88f47d55
Róbert Vári <mail.robertvari@gmail.com>
2021-01-10 14:56:08 +0100

    update

--- my_phonebook.py
+++ my_phonebook.py
@@ -1,17 +1,14 @@
+import time, json, os
+
 print("=" * 50, "My Phonebook", "=" * 50,)
+time.sleep(2)
+
 
 phonebook = {}
 
-name = input("Name:")
-phone = input("Phone:")
-address = input("Addres:")
-
-phonebook[phone] = {
-    "name": name,
-    "address": address
-}
-
-print(phonebook)
+if os.path.exists("phonebook.json"):
+    with open("phonebook.json") as f:
+        phonebook = json.load(f)
 
 name = input("Name:")
 phone = input("Phone:")
@@ -22,4 +19,8 @@ phonebook[phone] = {
     "address": address
 }
 
-print(phonebook)
+print("Saving data to database...")
+time.sleep(2)
+
+with open("phonebook.json", "w") as f:
+    json.dump(phonebook, f)
