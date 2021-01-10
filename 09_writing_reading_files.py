99b4a54674260f2068953fdb0e645d638c49d8be
Róbert Vári <mail.robertvari@gmail.com>
2021-01-10 14:22:55 +0100

    update

--- 09_writing_reading_files.py
+++ 09_writing_reading_files.py
@@ -1,5 +1,13 @@
-name = "\nTamás"
+name = "\nRobert"
 
-f = open("my_data.txt", "a")
-f.write(name)
-f.close()
+# f = open("my_data.txt", "a")
+# f.write(name)
+# f.close()
+
+# write file
+with open("my_data.txt", "a") as f:
+    f.write(name)
+
+# read file
+with open("my_data.txt") as f:
+    print(f.read())
